de470a36a4d74c060f86c796a66751cd292a8610
Yune Kotomi <yune-kotomi@users.noreply.github.com>
2015-10-23 23:05:45 +0900

    書きだしたディスクをマウントする機能を実装

---
+++ mountdisk.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# 書きだしたBDをマウント
+
+require 'tmpdir'
+require 'fileutils'
+
+Dir.chdir(ARGV[0]) do
+  images = Dir.glob('*.img.*').sort
+
+  if ARGV[1] == 'stop'
+    system "sudo umount /tmp/nakatsu-mount"
+    system "sudo cryptsetup luksClose nakatsu"
+    system "sudo mdadm --stop /dev/md/md0-nakatsu"
+  else
+    devices = images.map do |image|
+      device = `losetup -f`.strip
+      system "sudo losetup #{device} #{image}"
+      device
+    end
+    device = '/dev/md/md0-nakatsu'
+    system "sudo mdadm --assemble --force #{device} #{devices.join(' ')}"
+    system "sudo cryptsetup luksOpen #{device} nakatsu"
+    FileUtils.mkdir('/tmp/nakatsu-mount')
+    system "sudo mount /dev/mapper/nakatsu /tmp/nakatsu-mount"
+    system "nautilus /tmp/nakatsu-mount"
+  end
+end
